f453c7f6df1921b6f61355c7ac99d6afe138cbf7
Vilena <vilenaamanova@gmail.com>
2024-06-27 23:49:06 +0300

    init

--- index.js
+++ index.js
@@ -72,4 +72,4 @@ app.post('/web-data', async (req, res) => {
 })
 
 const PORT = 8000;
-app.listen(PORT, () => console.log(`Server started on PORT` + PORT));
+app.listen(PORT, () => console.log(`Server started on PORT ` + PORT));
